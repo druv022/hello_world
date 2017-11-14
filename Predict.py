Fix scaling bug
Saving the scaler object for reuse

Predict.py
@@ -2,30 +2,45 @@ import numpy as np
 import pickle
 from sklearn.neural_network import MLPRegressor
 from sklearn import model_selection
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 
 def predict(x_predict, y_test):
     filename = "TrainedModel.p"
     model = pickle.load(open(filename, 'rb'))
 
-    scaler = StandardScaler()
+    filename = "TrainScale.p"
+    scaler = pickle.load(open(filename, 'rb'))
 
-    scaler.fit(x_predict)
+
+    print("x_predict",x_predict)
     x_predict = scaler.transform(x_predict)
+    print("x_predict", x_predict)
 
     y_predict = model.predict(x_predict)
     print("y_predict:", y_predict)
     print("y_test:", y_test)
     return y_predict
 
-x_predict =[-0.00153712,0.00202408,0.00470084,14.9698,15.2133,15.9578,17.3327,19.6191,23.4201,30.1854,44.3415,88.5689,200,84.3097,43.386,29.8183,23.2531,19.5439,17.3088,15.9678,15.2498,15.0305]
+x_predict =[-0.0379823,-5.62E-05,4.30E-04,5.00028,5.0778,5.32202,5.77526,6.52976,7.78305,10.008,14.6372,28.8659,200,28.7221,14.6009,9.99199,7.7742,6.52431,5.77175,5.31976,5.07646,4.99972]
+x_predict1 = [97.1831,-0.525232,-0.0655026,7.6424,7.6712,7.93927,8.49036,9.42563,10.9559,13.5449,18.3768,29.3664,65.1841,27.2412,8.68478,5.41058,4.02927,3.29218,2.85748,2.59448,2.44415,2.37903]
+
 x_predict = np.array(x_predict)
+print(np.shape(x_predict))
 x_predict = x_predict.reshape(1,22)
+
+x_predict1 = np.array(x_predict1)
+print(np.shape(x_predict1))
+x_predict1 = x_predict1.reshape(1,22)
 
 #print(x_predict[0])
 #print(np.shape(x_predict))
-y_test = [1,0,-7.31E-04]
+y_test = [1,0,-1.64E-05]
+y_test1 = [1,0,0.001123673]
 y_test = np.array(y_test)
 y_test = y_test.reshape(1,3)
 
+y_test1 = np.array(y_test1)
+y_test1 = y_test1.reshape(1,3)
+
 predict(x_predict,y_test)
+predict(x_predict1,y_test1)

train_nn1.py
@@ -48,6 +48,12 @@ def read_dataset():
     print(x.shape, y.shape)
     print(np.all(np.isfinite(x)), np.all(np.isfinite(y)))
 
+    num = 0
+    if num < 5:
+        print("x:",x)
+        print("y",y)
+        num +=1
+
     return x, y
 
 
@@ -64,15 +70,18 @@ def train_mlp():
 
     scaler = StandardScaler()
     scaler.fit(x_train)
+
+    print("x_train", x_train)
     x_train = scaler.transform(x_train)
     x_test = scaler.transform(x_test)
+    print("x_train", x_train)
 
     model.fit(x_train, y_train)
 
-    return model.score(x_test, y_test), model
+    return model.score(x_test, y_test), model,scaler
 
 def get_model():
-    score, model_ = train_mlp()
+    score, model_, scaler_ = train_mlp()
 
     flag = True
     i = 0
@@ -80,12 +89,18 @@ def get_model():
         if score > 0.60:
             filename = "TrainedModel.p"
             pickle.dump(model_, open(filename, 'wb'))
+
+            filename = "TrainScale.p"
+            pickle.dump(scaler_, open(filename, 'wb'))
+
             print("Model saved: score", score)
             flag = False
             i=20
         else:
-            score, model_ = train_mlp()
+            score, model_, scaler_ = train_mlp()
             i += 1
 
 
 get_model()
+
+#read_dataset()